56871fba097f7639c6c964eb3e2eb1326da390f4
raichuvn11 <raichuvn11@gmail.com>
2025-04-03 17:22:59 +0700

    update

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-
-// Thêm route GET "/" để tránh lỗi "Cannot GET /"
-app.get('/', (req, res) => {
-  res.send('Socket.IO server is running!');
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "*" // Cho phép kết nối từ mọi nguồn
+  }
 });
 
-// Phần Socket.IO logic
+// Route gốc để test
+app.get('/', (req, res) => {
+  res.send('Socket.IO Chat Server is running!');
+});
+
+// Xử lý Socket.IO
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log('A user connected: ' + socket.id);
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', { 
+      id: socket.id, 
+      text: msg 
+    });
+  });
+
   socket.on('disconnect', () => {
-    console.log('User disconnected');
+    console.log('User disconnected: ' + socket.id);
   });
 });
 
-const PORT = 3000;
+// Khởi động server
+const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
